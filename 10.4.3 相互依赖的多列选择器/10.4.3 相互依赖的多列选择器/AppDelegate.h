2292ff8a2a0a100a1d6928a776ed71149784af4e
test <422538722@qq.com>
2017-04-16 15:46:53 +0800

    10.4.3
    相互依赖的多列选择器

---
+++ 10.4.3 相互依赖的多列选择器/10.4.3 相互依赖的多列选择器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  10.4.3 相互依赖的多列选择器
+//
+//  Created by 李维佳 on 2017/4/4.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
